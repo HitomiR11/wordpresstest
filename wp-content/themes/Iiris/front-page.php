c76b32f21e4a1b3efc9cb2141b7ef89a289dbca6
HitomiR11 <iiris.service@outlook.com>
2025-09-10 12:11:42 +0300

    The added comment

--- wp-content/themes/Iiris/front-page.php
+++ wp-content/themes/Iiris/front-page.php
@@ -3,8 +3,9 @@
 <section class="page-wrap">
     <div class="container">
 
+        <!--Show the title in front page-->
         <h1><?php the_title();?></h1>
-
+        <!--Show the section-content.php in front page-->
         <?php get_template_part('includes/section', 'content');?>
 
         <!--Search form template for front page. when searchform.php have then underneath line doesn't need-->
